16cb6c71a5bf61abc7e2004d837391335185ea20
malib357 <marcelorodriguesdecarvalho@gmail.com>
2021-06-14 10:21:42 -0300

    exercicios

---
+++ igualdadeifelse.js
@@ -0,0 +1,10 @@
+let linguagem="java";
+
+if (linguagem!="java")
+{
+    console.log("Aprenderei mais tarde");
+}
+else
+{
+    console.log("Estou aprendendo");
+}
---
+++ meuprimeiroif.js
@@ -0,0 +1,10 @@
+var status=false;
+
+if (status) 
+{
+    console.log("O valor é true - verdadeiro");
+}
+else
+{
+    console.log("O valor é false - falso");
+}
